ee8c4f82c3717db4c773a522327cefc5cf259faa
Christopher Kuchman <kuchmanc@os1.engr.oregonstate.edu>
2019-11-09 15:29:02 -0800

    added exec input parsing

--- miniShell.c
+++ miniShell.c
@@ -6,10 +6,12 @@
 
 void exitShell(void);
 void cd(char *path);
-void execFunc(char *userInput);
+void forkFunc(char *userInput);
+void childExec(char *userInput);
 void childEndless(char *input);
 
 #define MAX_INPUT 2049
+#define MAX_ARG 512
 
 int main(int argc, char *argv[]) {
     //TODO testing fork limit
@@ -41,7 +43,7 @@ int main(int argc, char *argv[]) {
         } else {
             forkNum++;
             printf("Fork: %d PID: %d\n", forkNum, getpid());
-            execFunc(userInput);
+            forkFunc(userInput);
         }
 
         //TODO testing fork bomb protection
@@ -83,8 +85,7 @@ void cd(char *path) {
 }
 
 
-void execFunc(char *userInput) {
-    //TODO Temp child fork
+void forkFunc(char *userInput) {
     pid_t childPid = -5;
 
     childPid = fork();
@@ -95,14 +96,64 @@ void execFunc(char *userInput) {
             exit(1);
             break;
         case 0:
-            childEndless(userInput);
-            printf("child");
+            //childEndless(userInput);
+            childExec(userInput);
+            //printf("child");
             exit(1);
             break;
         default:
             break;
     }
 }
+
+
+void childExec(char *userInput) {
+    char *str = NULL;
+    char *command = NULL;
+    char *input = NULL;
+    char *output = NULL;
+    char *arg[MAX_ARG];
+    int background = 0;
+
+    int i;
+    for (i = 0; i < MAX_ARG; i++) {
+        arg[i] = NULL;
+    } 
+
+    command = strtok(userInput, " ");
+
+    str = strtok(NULL, " ");
+   
+    int argNum = 0; 
+    while (str != NULL) {
+        if (strcmp(str, "<") == 0) {
+            input = strtok(NULL, " ");
+        } else if (strcmp(str, ">") == 0) {
+            output = strtok(NULL, " ");
+        } else if (strcmp(str, "&") == 0) {
+            background = 1;
+        } else {
+            arg[argNum] = str;
+            argNum++;
+        }
+
+        //Get next word
+        str = strtok(NULL, " ");
+    }
+
+    printf("Command: %s\n", command);
+    printf("Input: %s\n", input);
+    printf("Output: %s\n", output);
+    printf("Background: %d\n", background);
+
+    i = 0;
+    while (arg[i][0] != '\n') {
+        printf("Argument %d: %s\n", i, arg[i]);
+        i++; 
+   }
+
+}
+
 
 
 void childEndless(char *input) {
