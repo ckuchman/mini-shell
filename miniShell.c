c52ffaf0cef7c4e1169f563fda7771f9fb9453ba
Christopher Kuchman <kuchmanc@os1.engr.oregonstate.edu>
2019-11-09 12:26:56 -0800

    Added startng kill and cd methods and have forking function

--- miniShell.c
+++ miniShell.c
@@ -1,31 +1,113 @@
+#include<sys/types.h>
+#include<unistd.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-void exit();
-void cd();
+void exitShell(void);
 void cd(char *path);
+void execFunc(char *userInput);
+void childEndless(char *input);
 
+#define MAX_INPUT 2049
 
 int main(int argc, char *argv[]) {
+    //TODO testing fork limit
+    int forkLimit = 2;
+    int forkNum = 0;
 
-    printf("test");
+    //Get memory block for input
+    char *userInput;
+    userInput = (char*)malloc(sizeof(char) * MAX_INPUT);
+
+    //Main shell loop
+    while (strcmp(userInput, "exit") != 0) {
+
+        //Take in user input
+        printf(": ");
+        fgets(userInput, MAX_INPUT, stdin);
+
+        //Clears new line
+        if (userInput[strlen(userInput) - 1] == '\n') {
+            userInput[strlen(userInput) - 1] = '\0';
+        }
+
+        if (strcmp(userInput, "cd") == 0) {
+            cd("");
+        } else if (strncmp(userInput, "cd ", 3) == 0) {
+            cd(userInput + 3);
+        } else if (strcmp(userInput, "status") == 0) {
+            //TODO status function
+        } else {
+            forkNum++;
+            printf("Fork: %d PID: %d\n", forkNum, getpid());
+            execFunc(userInput);
+        }
+
+        //TODO testing fork bomb protection
+        if (forkNum > forkLimit) {
+            abort();
+        }
+    }
+
+    // Take care of children processes
+    exitShell();
+
+    //Clear memory
+    free(userInput);
 
     return 0;
-
 }
 
-void exit() {
-    //Kill all other processes currently running started by the shell
 
-
-    //Kills the current process
-
+void exitShell(void) {
+    //TODO Kill all other processes currently running started by the shell
+    printf("exit run");
 }
 
-void cd(){
-    //When no value is passed in it uses a overlaoded function? TODO
-    cd("TODO");
+
+void cd(char *path) {
+    char *truePath;
+    truePath = (char*)malloc(sizeof(char) * MAX_INPUT);
+
+    //If path is empty cd to HOME otherwise go to location
+    if (strcmp(path, "") == 0) {
+        printf("no input, using HOME");
+
+        truePath = getenv("HOME");
+    } else {
+        truePath = path;
+    }
+
+    chdir(truePath);
 }
 
-void cd(char *path){
 
-)
+void execFunc(char *userInput) {
+    //TODO Temp child fork
+    pid_t childPid = -5;
+
+    childPid = fork();
+    switch (childPid)
+    {
+        case -1:
+            perror("Fork Failure");
+            exit(1);
+            break;
+        case 0:
+            childEndless(userInput);
+            printf("child");
+            exit(1);
+            break;
+        default:
+            break;
+    }
+}
+
+
+void childEndless(char *input) {
+    while (1) {
+        printf("%s\n", input);
+        sleep(3);
+    }
+}
