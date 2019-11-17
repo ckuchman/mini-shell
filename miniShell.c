9f5ff3d3a43fc8cfc5cb86bf344f19317048b68b
Christopher Kuchman <kuchmanc@os1.engr.oregonstate.edu>
2019-11-16 16:34:29 -0800

    Added status and proper exit returns

--- miniShell.c
+++ miniShell.c
@@ -12,16 +12,22 @@
 
 
 void cd(char *path);
-pid_t forkFunc(char *arg[MAX_ARG], char *input, char *output, int background);
+pid_t forkFunc(char *arg[MAX_ARG], char *input, char *output, int background, int *childExitMethod);
 void expandString(char *str, int strLen);
 void parseCommand(char *userInput, char *arg[MAX_ARG], char **input, char **output, int *background);
 
+//TODO List
+//should look at PATH for commands?
+//Print the process of a background process when it begins
+//Print a message of background process and exit status when it ends
+//Implement signals
 
 int main(int argc, char *argv[]) {
     //Child tracker
     int childCount = 0;
     pid_t childVal;
-    int *childExitMethod;
+    int fgExitMethod = -10; 
+    int childExitMethod, exitStatus;
 
     pid_t *childs = (pid_t*)malloc(sizeof(pid_t) * MAX_THREADS);
     
@@ -62,7 +68,21 @@ int main(int argc, char *argv[]) {
         } else if (strncmp(userInput, "cd ", 3) == 0) {
             cd(userInput + 3);
         } else if (strcmp(userInput, "status") == 0) {
-            //TODO status function
+            if (fgExitMethod == -10) {
+                printf("exit status 0?\n");
+            } else {
+
+                if (WIFEXITED(fgExitMethod)) {
+                    exitStatus = WEXITSTATUS(fgExitMethod);
+                    printf("exit status %d\n", exitStatus);
+                }
+
+                if (WIFSIGNALED(fgExitMethod)) {
+                    exitStatus = WTERMSIG(fgExitMethod);
+                    printf("terminated by signal %d\n", exitStatus);
+                }
+
+            }
         } else if (strcmp(userInput, "exit") != 0) {
            //printf("Fork: %d PID: %d\n", forkNum, getpid());
 
@@ -72,9 +92,10 @@ int main(int argc, char *argv[]) {
             if (userInput[0] != '#') {
                 //Run command
                 if (childCount < MAX_THREADS) {
-                    childPid = forkFunc(arg, input, output, background);
+                    childPid = forkFunc(arg, input, output, background, &fgExitMethod);
 
                     if (childPid > 0) {
+
                         childCount++;
 
                         //Store the child pid in the child array
@@ -137,7 +158,7 @@ void cd(char *path) {
 }
 
 
-pid_t forkFunc(char *arg[MAX_ARG], char *input, char *output, int background) {
+pid_t forkFunc(char *arg[MAX_ARG], char *input, char *output, int background, int *childExitMethod) {
     pid_t childPid = -5;
 
     childPid = fork();
@@ -148,35 +169,43 @@ pid_t forkFunc(char *arg[MAX_ARG], char *input, char *output, int background) {
             exit(1);
             break;
         case 0:
-            if (input != NULL) {
+            if (input != NULL || background != 0) {
 
-                printf("input:%s\n", input);
+                if (input == NULL) {
+                    input = "/dev/null";
+                }
 
                 int inputFile = open(input, O_RDONLY);
 
                 if (inputFile < 0) {
                     printf("Failure to open input file\n");
+                    exit(1);
                 }
 
                 if (dup2(inputFile, 0) < 0) {
                     printf("Failed to dup input\n");
+                    exit(1);
                 }
 
                 close(inputFile);
             }
 
-            if (output != NULL) {
+            if (output != NULL || background != 0) {
 
-                printf("output:%s\n", output);
+                if (output == NULL) {
+                    output = "/dev/null";
+                }
 
                 int outputFile = open(output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
                 if (outputFile < 0) {
                     printf("Failure to open output file\n");
+                    exit(1);
                 }
 
                 if (dup2(outputFile, 1) < 0) {
                     printf("Failed to dup output\n");
+                    exit(1);
                 }
 
                 close(outputFile);
@@ -196,7 +225,7 @@ pid_t forkFunc(char *arg[MAX_ARG], char *input, char *output, int background) {
         default:
             //If not a background process then wait for the process
             if (background == 0) {
-                childPid = wait(NULL);
+                childPid = waitpid(childPid, childExitMethod, 0);
             }
             break;
     }
