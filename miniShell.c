d8d73465c0d133359eec3035c417001d976b960f
Christopher Kuchman <kuchmanc@os1.engr.oregonstate.edu>
2019-11-18 20:38:33 -0800

    Added a bunch more comments

--- miniShell.c
+++ miniShell.c
@@ -197,94 +197,123 @@ int main(int argc, char *argv[]) {
 
 
 pid_t forkFunc(char *arg[MAX_ARG], char *input, char *output, int background, int *childExitMethod) {
+    //This function forks off a process that runs the passed in execution along with any
+    //redirection wanted or if it should run in the background.
 
     pid_t childPid = -10;
 
+    //Forks off a process that runs the execution
     childPid = fork();
     switch (childPid)
     {
+        //Failed Fork
         case -1:
+
+            //If the fork fails then a error sent to errout
             perror("Fork Failure");
             fflush(stderr);
             exit(1);
             break;
+
+        //Child Process
         case 0:
+
+            //If a input is put in, then the input for the exectued process is redirected
+            //if the process is run in the background it will also make input from null if no input is provided
             if (input != NULL || background != 0) {
 
+                //If a background process and no input given, defaults to null
                 if (input == NULL) {
                     input = "/dev/null";
                 }
 
+                //Opens the input and reports a error if it fails
                 int inputFile = open(input, O_RDONLY);
-
                 if (inputFile < 0) {
                     printf("Failure to open input file\n");
                     fflush(stdout);
                     exit(1);
                 }
 
+                //Redirects stdin to the input file and reports a error if it fails
                 if (dup2(inputFile, 0) < 0) {
                     printf("Failed to dup input\n");
                     fflush(stdout);
                     exit(1);
                 }
 
+                //Closes input file
                 close(inputFile);
             }
 
+            //If a output is put in, then the output for the exectued process is redirected
+            //if the process is run in the background it will also make output from null if no output is provided
             if (output != NULL || background != 0) {
 
+                //If a background process and no output given, defaults to null
                 if (output == NULL) {
                     output = "/dev/null";
                 }
 
+                //Opens the output and reports a error if it fails
                 int outputFile = open(output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
                 if (outputFile < 0) {
                     printf("Failure to open output file\n");
                     fflush(stdout);
                     exit(1);
                 }
 
+                //Redirects stdout to the output file and reports a error if it fails
                 if (dup2(outputFile, 1) < 0) {
                     printf("Failed to dup output\n");
                     fflush(stdout);
                     exit(1);
                 }
 
+                //Closes output file
                 close(outputFile);
             }
 
-
+            //If this is not a background process then re-enable the ability to give a interrupt signal
             if (background == 0) {
-                //Handle shell signals
+
                 struct sigaction dft = {0};
                 dft.sa_handler = SIG_DFL;
                 sigaction(SIGINT, &dft, NULL);
+
             }
 
-            //Handle shell signals
+            //Set up the child to ignore the stop signal
             struct sigaction stp = {0};
             stp.sa_handler = SIG_IGN;
             sigaction(SIGTSTP, &stp, NULL); 
 
+            //Runs the execution and returns a error if it fails
             execvp(arg[0], arg);
             perror("Exec Failure!\n");
             fflush(stderr);
             exit(1);
             break;
+
+        //Parent Process
         default:
+
             //If not a background process then wait for the process
             if (background == 0) {
 
+                //wait for the process to finish
                 childPid = waitpid(childPid, childExitMethod, 0);
 
+                //Prints out the signal that killed the foreground process if it was
+                //singal terminated
                 if (WIFSIGNALED(*childExitMethod)) {
                     int exitStatus = WTERMSIG(*childExitMethod);
                     printf("terminated by signal %d\n", exitStatus);
                     fflush(stdout);
                 }
             } else {
+
+                //Prints what background process id was started
                 printf("Background process %d started\n", childPid);
                 fflush(stdout);
             }
@@ -296,19 +325,25 @@ pid_t forkFunc(char *arg[MAX_ARG], char *input, char *output, int background, in
 
 
 void expandString(char *str, int strLen) {
-    int pid = getpid();
+    //This function takes in a string and expands every instance of $$ to the process pid.
 
+    //Gets the current process pid and determines its string length
+    int pid = getpid();
     int pidLen = snprintf(NULL, 0, "%d", pid);
 
+    //Creates a temp string that stores the pid number as characters    
     char* pidStr = malloc(pidLen + 1);
     snprintf(pidStr, pidLen + 1, "%d", pid);
 
+    //Temp string that can be be copied from
     char* tempStr = (char*)malloc(sizeof(char) * MAX_INPUT);
     strcpy(tempStr, str);    
 
+    //Iterates over the string replacing $$ with the ID
     int i = 0, j = 0, k = 0;
     while (tempStr[i] != '\0') {
 
+        //If this is the start of a $$ then replace both with the ID
         if (tempStr[i] == '$' && tempStr[i+1] == '$') {
             while (pidStr[k] != '\0') {
                 str[j] = pidStr[k];
@@ -319,6 +354,8 @@ void expandString(char *str, int strLen) {
             k = 0;
             i += 2;
         } else {
+
+            //Write the current character if not a $$
             str[j] = tempStr[i];
 
             i++;
@@ -327,6 +364,7 @@ void expandString(char *str, int strLen) {
     }
     str[j] = '\0';
 
+    //Free the temp string memory
     free(pidStr);
     free(tempStr);
 }
